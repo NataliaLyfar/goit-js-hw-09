d1a1725c6064829d47da0495fd4c036896e5d358
Nataliia Lyfar <lifarnatala@gmail.com>
2022-05-16 21:02:23 +0300

    added timer without library

---
+++ src/js/04-timer-without-library.js
@@ -0,0 +1,25 @@
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+const deadlineDate = new Date('2022-05-22');
+    
+const timer = () => {
+    const today = new Date();
+    const delta = deadlineDate - today;
+    const seconds = Math.floor(delta / 1000 % 60);
+    const minutes = Math.floor(delta / 1000 / 60 % 60);
+    const hours = Math.floor(delta / 1000 / 60 / 60 % 24);
+    const days = Math.floor(delta / 1000 / 60 / 60 / 24);
+    const formatedSeconds = seconds >= 9 ? seconds : `0${seconds}`;
+    const formatedMinutes = minutes > 9 ? minutes : `0${minutes}`;
+    const formatedHours = hours > 9 ? hours : `0${hours}`;
+    const formatedDays = days > 9 ? days : `0${days}`;
+    dataHours.textContent = formatedHours;
+    dataMinutes.textContent = formatedMinutes;
+    dataDays.textContent = formatedDays;
+    dataSeconds.textContent = formatedSeconds;
+};
+setInterval(timer, 1000)
+
